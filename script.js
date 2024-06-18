8c08aa82b58e12f97ea03febdb29ac797075932e
Марк Антонов <dbasbkfga@Marks-MacBook-Air.local>
2024-06-18 12:04:45 +0300

    changes

--- script.js
+++ script.js
@@ -7,3 +7,18 @@ function closeModal(modalId) {
     document.getElementById(modalId).style.display = 'none';
     document.querySelector('.main-content').classList.remove('blur-background');
 }
+function openModal(modalId) {
+    document.getElementById(modalId).style.display = 'block';
+    document.querySelector('body').classList.add('blur-background');
+}
+
+function closeModal(modalId) {
+    document.getElementById(modalId).style.display = 'none';
+    document.querySelector('body').classList.remove('blur-background');
+}
+
+function closeModalOnClick(event, modalId) {
+    if (event.target.classList.contains('modal')) {
+        closeModal(modalId);
+    }
+}
